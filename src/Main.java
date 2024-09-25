dab7b09d3738903c0f505837fc5baa94b4421349
Xavier Glenn <xglenn@appdev.yearup.org>
2024-09-25 10:42:30 -0700

    Movie App Base 2
    Project guide for the work on 9/25/24

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+{public class Main {
+    public static void main ( String[] args )
+
+    String movieName = "Bray Wyatt: Becoming Immortal";
+    int yearReleased:2024;
+    String genre:'Documentary';
+    double 8.5/10;
+    boolean 'false';
+    char directorInitial:'C';
+
+System.out.println("Movie Name:"+movieName);
+System.out.println("Year Released:"+yearReleased);
+System.out.println("Genre:"+genre);
+System.out.println("IMDb Rating:"+imdbRating);
+System.out.println("Director's Last Name Initial:"+directorInitial);
+}
+
+String[] movieNames = {"Seven Pounds", "Interstellar", "Inception"};
+int[] movieYears = {2008, 2009, 2010}
+String[] favoriteGenres = {"Psychological Horror", "Comedy", "Animated"};
+}
+
+{
+System.out.println("First movie" + movieNames[0])
+System.out.println("Second Movie" + movieNames[1])
+System.out.println("Last movie" + movieNames[2])
+
+System.out.println("Favorite Genres:" + favoriteGenres[0]);
+System.out.println("Favorite Genres:" + favoriteGenres[1]);
+System.out.println("Favorite Genres:" + favoriteGenres[2]);
+System.out.println("Favorite Genres:" + favoriteGenres[3]);}
